59e0744adb2f65e74c6be40f205b2a331609d6c3
dzwdz <kg67199@gmail.com>
2021-02-22 12:42:02 +0100

    add a test replacement

---
+++ src/main/java/com/github/burgerguy/hudtweaks/ReplacementTest.java
@@ -0,0 +1,44 @@
+package com.github.burgerguy.hudtweaks;
+
+import com.github.burgerguy.hudtweaks.api.HudTweaksApi;
+import com.github.burgerguy.hudtweaks.api.PlaceholderName;
+import com.github.burgerguy.hudtweaks.hud.HTIdentifier;
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.Collection;
+
+// temporary class
+// todo: move to a test mod or something - remember to remove it from fabric.mod.json
+public class ReplacementTest implements HudTweaksApi {
+    @Override
+    public Collection<PlaceholderName> getCustomElementEntries() {
+        return ImmutableSet.of(new SomeReplacement());
+    }
+}
+
+class SomeReplacement extends PlaceholderName {
+    protected SomeReplacement() {
+        super(new HTIdentifier("test", "health", "square"), "onHealthRowsChange");
+    }
+
+    @Override
+    protected double calculateWidth(MinecraftClient client) {
+        return 20;
+    }
+
+    @Override
+    protected double calculateHeight(MinecraftClient client) {
+        return 20;
+    }
+
+    @Override
+    protected double calculateDefaultX(MinecraftClient client) {
+        return 100;
+    }
+
+    @Override
+    protected double calculateDefaultY(MinecraftClient client) {
+        return 100;
+    }
+}
